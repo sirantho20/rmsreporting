a918a81adf7766d3803fb9fc190c64e5b1e0ed69
aafetsrom <aafetsrom@HTG-LP-59.hta.local>
2014-06-10 14:39:28 +0000

    email test

--- test.php
+++ test.php
@@ -4,12 +4,12 @@ require 'vendor/autoload.php';
 
 $msg = Swift_Message::newInstance();
     $msg->setSubject('Power report-')
-            ->setFrom('aafetsrom@htghana.com')
+            ->setFrom('sirantho20@gmail.com')
             ->setTo('aafetsrom@htghana.com')
             ->setBody('Please find attached your requested report');
-    $transport = Swift_SmtpTransport::newInstance('10.3.0.30', '25');
-    $transport->setUsername('aafetsrom@htghana.com')
-            ->setPassword('!!AFtony19833');
+    $transport = Swift_SmtpTransport::newInstance('mail.google.com', '25');
+    $transport->setUsername('sirantho20@gmail.com')
+            ->setPassword('afTONY19833');
     
     $mail = new Swift_Mailer($transport);
     $mail->send($msg);
